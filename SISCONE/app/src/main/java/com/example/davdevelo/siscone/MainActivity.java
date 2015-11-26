9e00a36fe0741e59413b7e9094f1f66e4d144a98
davdevelo <davdevelo_dvd@hotmail.com>
2015-11-26 17:37:20 -0500

    Controlador

--- SISCONE/app/src/main/java/com/example/davdevelo/siscone/MainActivity.java
+++ SISCONE/app/src/main/java/com/example/davdevelo/siscone/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.davdevelo.siscone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Spinner sistemas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sistemas =(Spinner) findViewById(R.id.comboBoxTipoUSR);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this,R.array.tipoUsuario,android.R.layout.simple_spinner_item);
+        sistemas.setAdapter(adapter);
     }
+
+
+
 }
