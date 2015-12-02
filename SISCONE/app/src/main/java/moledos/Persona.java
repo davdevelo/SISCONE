326fc9d9a8809fe880abce317cb16593e5b8ffd6
Dancajas93 <dan.cajitas-93@hotmail.com>
2015-12-02 12:21:33 -0500

    Modelos y monentarios

---
+++ SISCONE/app/src/main/java/moledos/Persona.java
@@ -0,0 +1,64 @@
+package moledos;
+
+/**
+ * Created by DANIEL on 02/12/2015.
+ */
+public class Persona {
+
+    private String cedula;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String contrasena;
+
+    public Persona() {
+    }
+
+    public Persona(String cedula, String nombre, String apellido, String correo, String contrasena) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.contrasena = contrasena;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+}
---
+++ SISCONE/app/src/main/java/momentario/Listas.java
@@ -0,0 +1,14 @@
+package momentario;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import moledos.Persona;
+
+/**
+ * Created by DANIEL on 02/12/2015.
+ */
+public class Listas {
+
+    public static List<Persona> Profesores = new ArrayList<>();
+}
