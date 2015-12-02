c9b1afda49be378878fdadd7b234b2a6df9002b3
davdevelo <davdevelo_dvd@hotmail.com>
2015-12-02 12:24:18 -0500

    Version 2

--- SISCONE/app/src/main/java/com/example/davdevelo/siscone/RegistroProfesor.java
+++ SISCONE/app/src/main/java/com/example/davdevelo/siscone/RegistroProfesor.java
@@ -4,11 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class RegistroProfesor extends AppCompatActivity {
 
+    EditText cedulaProf;
+    EditText nombreProf;
+    EditText apellidoProf;
+    EditText correoProf;
+    EditText contraseñaProf;
+    EditText confContraseñaProf;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_profesor);
         findViewById(R.id.buttonRegresarLoginProf).setOnClickListener(new View.OnClickListener() {
@@ -24,4 +34,26 @@ public class RegistroProfesor extends AppCompatActivity {
             }
         });
     }
+
+    public void buscarElementos()
+    {
+        cedulaProf= (EditText) findViewById(R.id.editTextCedulaProf);
+        nombreProf= (EditText) findViewById(R.id.editNombreProf);
+        apellidoProf= (EditText) findViewById(R.id.editApellidoProf);
+        correoProf= (EditText) findViewById(R.id.editCorreoProf);
+        contraseñaProf= (EditText) findViewById(R.id.editContraseñaRegProf1);
+        confContraseñaProf= (EditText) findViewById(R.id.editContraseñaRegProf2);
+    }
+
+    public void limpiarRegistroProf ()
+    {
+        cedulaProf.setText("");
+        nombreProf.setText("");
+        apellidoProf.setText("");
+        correoProf.setText("");
+        correoProf.setText("");
+        contraseñaProf.setText("");
+        confContraseñaProf.setText("");
+    }
+
 }
